305fafc9330cf3d135ba29c77b6ce1f59a603231
dudaray <141421272+dudaray@users.noreply.github.com>
2024-05-20 20:46:25 -0300

    s

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ for (let i=0; i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-10T00:00:00")
+const tempoObjetivo1 = new Date("2024-12-10T00:00:00");
+let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
