1d9842037ee18561b46743f048afbd758e0bda5f
dudaray <141421272+dudaray@users.noreply.github.com>
2024-05-20 20:43:07 -0300

    s

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo")
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i=0; i <botoes.length;i++){
     botoes[i].onclick = function(){
@@ -13,3 +13,8 @@ for (let i=0; i <botoes.length;i++){
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-10")
+
+contadores[0].textContent = tempoObjetivo1;
