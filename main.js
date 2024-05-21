db7eee13ed469a0cc307c99eef5cc8656277fe25
dudaray <141421272+dudaray@users.noreply.github.com>
2024-05-20 21:01:51 -0300

    s

--- main.js
+++ main.js
@@ -26,5 +26,8 @@ function calculaTempo(tempoObjetivo){
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos +" segundos ";
 }
